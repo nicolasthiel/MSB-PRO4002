d0ca770996c909a22733f71bac0a3fcfc42464a0
Nicolas <68606261+nicolasthiel@users.noreply.github.com>
2025-01-09 14:14:04 +0100

    Added inital data pre-processing and filtering

--- script.R
+++ script.R
@@ -1,11 +1,18 @@
-required_packages <- c("BiocManager")
-for (pkg in required_packages) {
-    if (!require(pkg, character.only = TRUE)) {
-        install.packages(pkg, dependencies = TRUE)
-        library(pkg, character.only = TRUE)
-    }
-}
+#-----------------------------------------------------------------------------#
+# load and pre-process data
+#-----------------------------------------------------------------------------#
 
+# load data
 data_cpm <- read.delim("data/MAGNET_GeneExpressionData_CPM_19112020.txt", sep = "\t", row.names = 1)
 data_exon_lengths <- read.delim("data/MAGNET_exonLengths.txt", as.is = TRUE, row.names = 1)
 data_sample_info <- read.csv("data/MAGNET_SampleData_18112022.csv", row.names = 1)
+
+# convert binary columns to logical
+binary_columns <- c("afib", "VTVF", "Diabetes", "Hypertension")
+for (col in binary_columns) {
+    data_sample_info[[col]] <- data_sample_info[[col]] == "Yes"
+}
+
+# filter for samples with DCM or HCM
+filtered_sample_info <- data_sample_info[data_sample_info$etiology %in% c("DCM", "HCM"), ]
+filtered_cpm <- data_cpm[, rownames(filtered_sample_info)]
